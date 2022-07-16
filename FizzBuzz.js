ddaacbd0379ad4352bd8d542828e24d83929dc2b
PREDATOR <prateek772492@gmail.com>
2022-07-17 00:43:07 +0530

    FizzBuzz - 1

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -9,11 +9,11 @@ let count = 0;
 function fizzBuzz() {
 count += 1;
 
-if (count%3 === 0)
-output.push("Fizz");
+if(count % 3 === 0 && count % 5 === 0) output.push("FizzBuzz")
+
+else if (count%3 === 0) output.push("Fizz");
 
 else if(count%5 === 0) output.push("Buzz")
-
 
 else output.push(count)
 
